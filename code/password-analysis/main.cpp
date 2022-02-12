2f8f82b6c4635129877e0bfe482d2a2ad5e98195
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-02-12 14:37:22 +0000

    Update main.cpp

--- code/password-analysis/main.cpp
+++ code/password-analysis/main.cpp
@@ -17,8 +17,9 @@ std::string fixHex(std::string hexString){
     return newString;
 }
 
-bool checkAndFixHex(std::string& str){
+bool checkAndFixEncodings(std::string& str){
     // Some hex strings in one of the files seems to be marked with $HEX[(the string)], this should help fix strings from that specific file.
+    // I also need to find other ways to check for hex, and other such encodings.
     if (str.rfind("$HEX[", 0) == 0) { // pos=0 limits the search to the prefix
         str.erase(0,5);
         str.pop_back();
@@ -33,7 +34,7 @@ bool isASCII(const std::string str){
 
 bool check(std::string& str){
     // This tries to detect and fix hex strings
-    if(!checkAndFixHex(str)) {return false;}
+    if(!checkAndFixEncodings(str)) {return false;}
     if(!isASCII(str)) {return false;}
     return true;
 }
@@ -42,43 +43,57 @@ int main(int argc, char** argv){
     if(argc != 2){
         throw std::invalid_argument("Received invalid arguments");
     }
-    std::string fileString;
-    std::ifstream f(argv[1]);
-    if (!f.is_open()){
+    std::string lineString;
+    std::ifstream file(argv[1]);
+    if (!file.is_open()){
         std::cout << "Failed to open" << std::endl;
     }
 
     // These variables hold the statstics that we want to analyse.
     std::vector<unsigned int> lengths;
     lengths.resize(maxlen); // Anything above "maxlen" characters will go in a seperate variable.
-    unsigned int veryLong = 0;
+    unsigned int aboveMaxLen = 0;
     unsigned int ignored = 0;
 
     // Main loop where the analysis of the file will take place
-    while (getline (f, fileString)) {
-        if(check(fileString)){
-            int linelen = fileString.length();
+    while (getline (file, lineString)) {
+        if(check(lineString)){
+            int linelen = lineString.length();
             if (linelen < maxlen){
                 lengths[linelen]+=1;
             }else{
-                veryLong+=1;
+                aboveMaxLen+=1;
             }
         }else{
             ignored+=1;
         }
     }
-    f.close();
-
+    file.close();
 
     // Display statistics to the terminal
     for(int i=0; i<maxlen; i++){
         std::cout << i << ": " << lengths[i] << std::endl;
     }
-    std::cout << "Longer: " << veryLong << std::endl;
+    std::cout << "Longer: " << aboveMaxLen << std::endl;
     std::cout << "Ignored: " << ignored << std::endl;
 
-
-    // Write statistics out to a file which can be loaded in python for graphing and further analysis
+    // Save statstics
+    // One file per statistic should make life easiest
+    std::ofstream statsOutput;
+    statsOutput.open("output/lengths");
+    for(int i=0; i<maxlen; i++){
+        statsOutput << lengths[i];
+        if(i != maxlen-1){
+            statsOutput << ",";
+        }
+    }
+    statsOutput.close();
+    statsOutput.open("output/aboveMaxLen");
+    statsOutput << aboveMaxLen;
+    statsOutput.close();
+    statsOutput.open("output/ignored");
+    statsOutput << ignored;
+    statsOutput.close();
 
     return 0;
 }
